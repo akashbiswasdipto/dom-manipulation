dc797d918a44fd0c2648b07f0102046358aaa394
Akash Biswas <akashbiswas.dipto@gmail.com>
2018-06-04 04:19:20 +0000

    task1 2 3 4ab

--- script.js
+++ script.js
@@ -36,3 +36,9 @@ $('#addText4').click(function(event){
     var paragraph= $('<p>').text('Hello World')
     $('#task4a').append(paragraph)
 })
+
+//task4b
+function changeBG(event){
+    var color = event.target.innerText.toLowerCase();
+    $('body').css('background-color',color)
+}
