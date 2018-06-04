3505ae94278ecf3ce6f78a08e4c47afd97b5db83
Akash Biswas <akashbiswas.dipto@gmail.com>
2018-06-04 04:01:49 +0000

    task1 2 3 4

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 /* Task 1 */
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
+/* Task 2 */
+//task2a
+var textBtn= document.getElementById('TextBtn2')
+textBtn.addEventListener('click',function(){
+    var task2 = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText="Hello World"
+    task2.appendChild(paragraph)
+    
+})
 
+//task2b
+function changeBGcolor(event){
+    var body= document.getElementsByTagName('body')[0]
+    if(event.target.innerText=="red"){
+        body.style.backgroundColor='red'
+    }
+    if(event.target.innerText=="green"){
+        body.style.backgroundColor='green'
+    }
+}
 
-/* Task 2 using plain JavaScript */
+//task2c
 
-
-
+function redirectToGoogle(){
+    window.location.assign('http://google.com')
+}
 /* Task 4 using jQuery */
